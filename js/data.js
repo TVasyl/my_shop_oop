0b410d1d96e1a0e4df90b9acb60b94e677192b87
Vasyl Trots <vaska.trots@gmail.com>
2022-09-21 22:06:58 +0300

    Change path to image files in data

--- js/data.js
+++ js/data.js
@@ -1,52 +1,52 @@
 const data = {
     "card-1": {
         name: "Card 1",
-        image: "/images/control-panel-icon.png",
+        image: "images/control-panel-icon.png",
         price: 100
     },
     "card-2": {
         name: "Card 2",
-        image: "/images/drum-icon.png",
+        image: "images/drum-icon.png",
         price: 200
     },
     "card-3": {
         name: "Card 3",
-        image: "/images/guitar-icon.png",
+        image: "images/guitar-icon.png",
         price: 300
     },
     "card-4": {
         name: "Card 4",
-        image: "/images/keyboard-piano-icon.png",
+        image: "images/keyboard-piano-icon.png",
         price: 400
     },
     "card-5": {
         name: "Card 5",
-        image: "/images/microphone-voice-icon.png",
+        image: "images/microphone-voice-icon.png",
         price: 500
     },
     "card-6": {
         name: "Card 6",
-        image: "/images/stereo-speaker-icon.png",
+        image: "images/stereo-speaker-icon.png",
         price: 600
     },
     "card-7": {
         name: "Card 7",
-        image: "/images/trombone-icon.png",
+        image: "images/trombone-icon.png",
         price: 700
     },
     "card-8": {
         name: "Card 8",
-        image: "/images/vinyl-icon.png",
+        image: "images/vinyl-icon.png",
         price: 800
     },
     "card-9": {
         name: "Card 9",
-        image: "/images/control-panel-icon.png",
+        image: "images/control-panel-icon.png",
         price: 900
     },
     "card-10": {
         name: "Card 10",
-        image: "/images/drum-icon.png",
+        image: "images/drum-icon.png",
         price: 1000
     }
     
@@ -56,17 +56,17 @@ const data = {
 const advertising = {
     "add-1": {
         "title": "You can to add the advertising in this place #1",
-        "image": "/images/control-panel-icon.png",
+        "image": "images/control-panel-icon.png",
         "style": "advertising"
     },
     "add-2": {
         "title": "You can to add the advertising in this place #2",
-        "image": "/images/drum-icon.png",
+        "image": "images/drum-icon.png",
         "style": "advertising"
     },
     "add-3": {
         "title": "You can to add the advertising in this place #3",
-        "image": "/images/keyboard-piano-icon.png",
+        "image": "images/keyboard-piano-icon.png",
         "style": "advertising"
     }
 
